68128e3f2e42d2d05c1870b5f4018bce32c0ea25
bradcerenzia <brad@zesty.net>
2017-10-26 14:47:14 -0700

    More cleanup

---
+++ Assets/Scripts/PlaybackOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlaybackOptions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Application.LoadLevel(1);
+    }
+}
+
---
+++ Assets/Scripts/ShowApple.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowApple : MonoBehaviour {
+
+    public GameObject fruit;
+    public GameObject CanvasObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // make apple visible
+        fruit.SetActive(true);
+  
+        // enable scroll of credits
+        CanvasObject.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        fruit.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
